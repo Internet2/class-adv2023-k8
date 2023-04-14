7c7e53eab1043cfc3ec32af8628420a870e67f20
Kraig Eisenman <68161631+kraigeisenman@users.noreply.github.com>
2023-04-14 10:39:58 -0400

    Update outputs.tf

--- kraige/outputs.tf
+++ kraige/outputs.tf
@@ -1,6 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+*/Output variables are a way to extract information about the resources that Terraform 
+manages so that you can use it elsewhere, like in another Terraform configuration or to display it to the user. /*
+  
+
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
