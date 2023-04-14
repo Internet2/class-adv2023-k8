6cb5a9d88697aa279cfdb1a5117b6cb9c3402289
Kraig Eisenman <68161631+kraigeisenman@users.noreply.github.com>
2023-04-14 10:31:39 -0400

    Update terraform.tf

--- kraige/terraform.tf
+++ kraige/terraform.tf
@@ -9,6 +9,19 @@ terraform {
     }
   }
   */
+
+ /* This block specifies the required providers and their versions for the Terraform configuration. It includes:
+- The AWS provider (hashicorp/aws) with version constraint `~> 4.47.0`
+- The Random provider (hashicorp/random) with version constraint `~> 3.4.3`
+- The TLS provider (hashicorp/tls) with version constraint `~> 4.0.4`
+- The CloudInit provider (hashicorp/cloudinit) with version constraint `~> 2.2.0`
+
+
+This line sets the required Terraform version for the configuration. It specifies a version constraint of `~> 1.3`, meaning it requires a Terraform version that is compatible with version 1.3, but not necessarily the exact version.
+
+In summary, this configuration file sets up the required providers and their versions and specifies the required Terraform version for the project.
+*/
+    
   required_providers {
     aws = {
       source  = "hashicorp/aws"
