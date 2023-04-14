9b02f8fbeb2792c6b3207125ea6ee8832290a98a
Kraig Eisenman <68161631+kraigeisenman@users.noreply.github.com>
2023-04-14 10:57:25 -0400

    Update variables.tf

--- kraige/variables.tf
+++ kraige/variables.tf
@@ -1,6 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+# Use this document to set variables.  The region for example, can be set by changing the default variable.
+
 variable "region" {
   description = "AWS region"
   type        = string
